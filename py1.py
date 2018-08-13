411fc1d38d07c6b69d5be4112149486c964ad991
Ana Beatriz Martins <36774340+AnaBeatrizMart@users.noreply.github.com>
2018-08-13 13:19:12 -0300

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,23 @@
+## Primeiros passos em python
+## Exercicios realizados durante a aula de Python do Curso em Video
+## Professor Gustavo Guanabara
+## Executados em Agosto de 2018
+
+## Para mostrar/printar frases na tela é indispensavel o uso de aspas
+print ('Hello, world')
+Hello, world
+ 
+## A soma utiliza uma estrutura parecida, mas não usas aspas
+print (5+7)
+print (8+9)
+print(26+99)
+print (50+50)
+print (18+18)
+
+## Testando a soma utilizando aspas
+print ('7'+'4')
+
+## Calculos sem o comando print
+7+4
+5+8+8
+
